826d277ab1711558e06120b824d2db04c97185f3
marcosfpr <marcos.rezende@aluno.ufop.edu.br>
2021-04-22 17:22:45 -0300

    Introduction and Linking

---
+++ .ccls-cache/@home@marcos@Estudos@cmake/main.cpp
@@ -0,0 +1,8 @@
+
+#include <vector>
+
+int main() {
+  std::vector<int> vint = { 1, 2, 3, 4 };
+
+  return 0;
+}
---
+++ 2. Linking/object/build/CMakeFiles/hello-world.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marcos/Estudos/cmake/2. Linking/object/hello.cpp" "/home/marcos/Estudos/cmake/2. Linking/object/build/CMakeFiles/hello-world.dir/hello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/marcos/Estudos/cmake/2. Linking/object/build/CMakeFiles/message-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
